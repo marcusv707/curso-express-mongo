7411bc65b33c004d587bef29e0b86b2a5bd18239
Marcus Aguiar <marcusv707@gmail.com>
2024-07-13 13:08:39 -0300

    Conectando no banco de dados

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// import http from "http";
+import 'dotenv/config';
 import app from "./src/app.js";
 
 const PORT = 3000;
@@ -8,11 +8,6 @@ const rotas = {
   "/livros": "Entrei na rota livros",
   "/autores": "Entrei na rota autores"
 }
-
-// const server = http.createServer((req, res) => {
-//   res.writeHead(200, {"Content-Type": "text/plain"});
-//   res.end(rotas[req.url]);
-// });
 
 app.listen(PORT, () => {
   console.log("Servidor escutando!");
